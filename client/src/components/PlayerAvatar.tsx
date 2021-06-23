e3a854bef6ed2132cd3a4890a173a34b715f2835
mdever <disbrow.6@gmail.com>
2021-06-22 23:58:00 -0400

    Default to showing user avatar

--- client/src/components/PlayerAvatar.tsx
+++ client/src/components/PlayerAvatar.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function PlayerAvatar(props: {username: string}) {
-    const [useDefaultAvatar, setUseDefaultAvatar] = useState(true);
+    const [useDefaultAvatar, setUseDefaultAvatar] = useState(false);
 
     return (
         <img alt="User Avatar" onError={() => setUseDefaultAvatar(true)} src={useDefaultAvatar ? '/user.png' : `/api/users/${props.username}/avatar`} style={{height: '80px'}}></img>
